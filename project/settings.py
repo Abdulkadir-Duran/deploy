cd21bd79032fa6217f158e6b3fac51e4e90658bb
Abdulkadir Duran Adan <boom@booms-Mac-mini.local>
2023-06-26 20:36:53 +0800

    first commit

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6*@8t4=^xo1j!_&0+00tx2m3m5^1)5zm85g!gn-%1(pf%k=s0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
